56f4bc01c5f101352b75fae05de80663b6bfff6a
lklimkiewicz <lukaszklimkiewicz@mac-mini-ukasz.local>
2016-12-04 21:17:41 +0100

    Initial Commit

---
+++ stack-view-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stack-view-example
+//
+//  Created by Łukasz Klimkiewicz on 04.12.2016.
+//  Copyright © 2016 Łukasz Klimkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
